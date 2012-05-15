735b0e466de28835e3324c3759fd7f585fd72761
Michael Hauspie <michael.hauspie@lifl.fr>
2012-05-15 11:45:29 +0200

    Added DAC driver files

---
+++ rflpc17xx/drivers/dac.h
@@ -0,0 +1,45 @@
+/* This file is part of rflpc. Copyright 2010-2011 Michael Hauspie
+ *
+ * rflpc is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * rflpc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with rflpc.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/** @file
+ * Digital to Analogic Converter
+ */
+/*
+    Author: Michael Hauspie <michael.hauspie@univ-lille1.fr>
+    Created: 2012-05-15
+
+    This files defines the driver for the Digita to Analogic Converter (p. 840)
+ */
+
+#ifndef __RFLPC_DAC_H__
+#define __RFLPC_DAC_H__
+
+#ifdef RFLPC_CONFIG_ENABLE_DAC
+
+#include "../nxp/LPC17xx.h"
+#include "../interrupt.h"
+
+/** @addtogroup dac Digital to Anologic Converter
+ * @ingroup drivers
+ * 
+ * @{
+ */
+
+
+
+/** @} */
+#endif /* ENABLE_DAC */
+
+#endif
